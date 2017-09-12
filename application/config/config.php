ec803faac70826b5dbf651870825b70a34eed99d
ramon <ramon@192.168.25.18>
2017-09-12 11:54:59 -0300

    Correção dos erros da primeira etapa do projeto.

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'C:\Users\ramon\OneDrive\Documentos\NetBeansProjects\tccli
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = 'index.html';
 
 /*
 |--------------------------------------------------------------------------
